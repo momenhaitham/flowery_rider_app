f2f9120b8d243006a9865f86bf6872c81d9a25c7
momenhaitham <momenhaithamcod12@gmail.com>
2026-02-09 08:12:54 +0200

    feat: Enhance onboarding screen with localized text and improved image display

--- lib/app/feature/onboarding/presentation/onboarding_screen.dart
+++ lib/app/feature/onboarding/presentation/onboarding_screen.dart
@@ -8,15 +8,18 @@ import 'package:provider/provider.dart';
 class OnboardingScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
+    //var width = MediaQuery.of(context).size.width;
+    var height = MediaQuery.of(context).size.height;
     AppProvider appProvider = Provider.of<AppProvider>(context);
     return Scaffold(
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           
-          Image.asset(AssetsImage.onBoaedingLogo),
-          Text(AppLocale.Welcometo.tr()),
-          Text(AppLocale.Floweryriderapp.tr()),
+          Image.asset(AssetsImage.onBoaedingLogo,fit: BoxFit.fill,height: height*0.3,),
+          Text(AppLocale.Welcometo.tr(),style: Theme.of(context).textTheme.headlineLarge,),
+          Text(AppLocale.Floweryriderapp.tr(),style: Theme.of(context).textTheme.headlineLarge,),
+          SizedBox(height: height*0.03,),
           ElevatedButton(onPressed: () {
             appProvider.changeLocale(context,"en");
           }, child: Text("change lang"))
