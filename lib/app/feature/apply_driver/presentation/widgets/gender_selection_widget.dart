3819682f50b9d7e873ec903ecd821ab3ea6bf37f
Ahmed Yousef <Ahmed.yf.el.sayed@example.com>
2026-02-21 22:28:42 +0200

    fix: add missing AppLocale import in gender_selection_widget

--- lib/app/feature/apply_driver/presentation/widgets/gender_selection_widget.dart
+++ lib/app/feature/apply_driver/presentation/widgets/gender_selection_widget.dart
@@ -1,4 +1,5 @@
 import 'package:easy_localization/easy_localization.dart';
+import 'package:flowery_rider_app/app/core/app_locale/app_locale.dart';
 import 'package:flowery_rider_app/app/core/resources/app_colors.dart';
 import 'package:flowery_rider_app/app/core/resources/font_manager.dart';
 import 'package:flutter/material.dart';
